0677a8e83f485d5ed527eb07ce2c84808d28d355
Uyen Tran <tranphuonguyen1011@gmail.com>
2025-07-14 15:04:42 +0100

    Test whole machine

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from Plugboard import *
+from Rotor import *
+from Reflector import *
+from EnigmaMachine import *
 
 if __name__ == "__main__":
-    plugboard = Plugboard()
-    plugboard.add(PlugLead("AB"))
-    plugboard.add(PlugLead("UI"))
+    plugboard2 = Plugboard()
+    pairs = ["PC", "XZ", "FM", "QA", "ST", "NB", "HY", "OR", "EV", "IU"]
+    for pair in pairs:
+        plugboard2.add(PlugLead(pair))
+    rotor1 = Rotor("I", "05", "P") # notch: Q
+    rotor2 = Rotor("Beta", "03", "G") # notch: None
+    rotor3 = Rotor("V", "24", "Z") # notch: Z
+    rotor4 = Rotor("IV", "18", "E") # notch: J
+    reflector = Reflector("A")
+    enigma = EnigmaMachine(plugboard2, [rotor1, rotor2, rotor3, rotor4], reflector)
+    
+    message = "BUPXWJCDPFASXBDHLBBIBSRNWCSZXQOLBNXYAXVHOGCUUIBCVMPUZYUUKHI"
+    encoded_message = enigma.encode(message)
+    print(encoded_message) 
 
-
-    print(plugboard.encode("Y"))
+    
